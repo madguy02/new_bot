c81b63f857d201255c374f0464ad247d06c91f33
madguy02 <manishkakoti688@gmail.com>
2017-01-02 12:10:32 +0530

    added

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAATZC3u0bI38BAGCFPrKTIKAKoe3UV33CWSAeZBLuw4QMN2YbzbCwmzIDz8x7pr7EbpCZBHuVbfWYjSPBejro5wfiHq7IE8YGgoThMmtOWNJhpKPJHixJE4ZBV1H5kRbpuG8DJOTCF9h9fHHJEuVhxzieWUKSQdm6UNH0npPSgZDZD"
