0f61e588be7073c7025332a7462ced8b951ca7aa
madguy02 <manishkakoti688@gmail.com>
2017-01-04 11:29:24 +0530

    generic message

--- index.js
+++ index.js
@@ -63,4 +63,54 @@ function sendTextMessage(sender, text) {
     })
 }
 
+
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 const token ="EAATZC3u0bI38BAGCFPrKTIKAKoe3UV33CWSAeZBLuw4QMN2YbzbCwmzIDz8x7pr7EbpCZBHuVbfWYjSPBejro5wfiHq7IE8YGgoThMmtOWNJhpKPJHixJE4ZBV1H5kRbpuG8DJOTCF9h9fHHJEuVhxzieWUKSQdm6UNH0npPSgZDZD"
